dee25566e5000654b4e0079fff1fe4a77f534980
1021048313 <1021048313@qq.com>
2021-05-23 19:43:35 +0800

    index页面轻微调整

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,7 +1,7 @@
 //index页面
 //使用promise
-import{request} from "../../request/index.js"
-const app = getApp()
+// import{request} from "../../request/index.js"
+const app = getApp();
 Page({
   data: {
     // 队伍信息
@@ -11,30 +11,27 @@ Page({
     userID: '28ee4e3e60867cd612781c4e6bd0ae26',
 
     tabs:[],
-
     userInfo: {},
     hasUserInfo: false,
     canIUseGetUserProfile: false,
   },
   //加载时发生：判断本地存储，获取队伍信息
   onLoad: function() {
+    this.getUserProfile();
 
-    
-    //根据当前用户，判断加载全部还是个人队伍页面
-    //这里先加载全部
     this.getteaminfo();  
     var that = this  
-    const db = wx.cloud.database()
+    const db = wx.cloud.database();
     db.collection("Label").get()
     .then(res=>{
-      console.log(res.data)
+
       that.setData({
         tabs: res.data
       })
     })
 
 
-    this.getUserProfile()
+
    
   },
 
@@ -64,44 +61,14 @@ Page({
 
 
 
-  //获取当前人数的函数
-  getmembernumber(team_id){
-    var that = this;
-    const db = wx.cloud.database();
-    //获取队伍member
-    const members = db.collection('Member');
-      members.where({
-        team_id:team_id
-      }).get({
-        success:function(res){
-          return res.data.length;
-        }
-      })
-    },
 
   // 获取队伍信息的函数
-  //修改1：增加参数
   getteaminfo()
   {
-
-    console.log("ddddddddddd");
-    // const db = wx.cloud.database();
-    // const teams = db.collection('Team');
-    // // var team_ids=[];
-    // teams.get().then(res=>{
-    //   console.log(res);
-    //   // console.log(res.data[0].deadline.toLocaleDateString());
-    //   for(var a=0;a<res.data.length;a++)
-    //   {
-    //     res.data[a].deadline=res.data[a].deadline.toLocaleDateString();
-    //     // team_ids.push(res.data[a]._id);
-    //   }
-
     var that=this;
     const db = wx.cloud.database();
     const team = db.collection('Team');
 
-    //条件1：全部
     team.where({
       isOver: false
     }).get().then(res=>{
@@ -110,13 +77,10 @@ Page({
           //日期转换
           if (res.data[a].deadline != null)
             res.data[a].deadline=res.data[a].deadline.toLocaleDateString();
-          //获取当前队员人数
-          // res.data[a].current_member=that.getmembernumber(res.data[a]._id);
         }
         this.setData({
             teaminfo:res.data,
           })
-          console.log(res.data)
       })
       .catch(err=>{
         console.log(err);
